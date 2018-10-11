f0ed060f6ffe2baa6319830967428ba19087b260
angeliquecd <acharles-davis00@stuy.edu>
2018-10-11 10:04:39 -0400

    made changes to Point so it would run with Driver

--- Point.java
+++ Point.java
@@ -1,27 +1,15 @@
 public class Point{
   private double x,y;
-<<<<<<< HEAD
-
-=======
->>>>>>> 9a8606a665c744760f1da3da449935ad9290be85
   public Point(double X, double Y){
     x=X;
     y=Y;
   }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 9a8606a665c744760f1da3da449935ad9290be85
   public Point(Point p){
     x= p.x;
     y= p.y;
   }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 9a8606a665c744760f1da3da449935ad9290be85
   public double getX(){
     return x;
   }
@@ -29,18 +17,6 @@ public class Point{
   public double getY(){
     return y;
   }
-<<<<<<< HEAD
-  public static double distance(Point a, Point b){
-
-  }
-  public double distanceTo(Point b){
-    math.sqrt ((this.point.x-b.x)*(this.point.x-b.x))
-  }
-}
-
-
-}
-=======
   public double distanceTo(Point a){
     return java.lang.Math.sqrt(java.lang.Math.pow((this.getX()-a.getX()),2)+java.lang.Math.pow((this.getY()-a.getY()),2));
   }
@@ -51,4 +27,3 @@ public class Point{
     return "("+this.getX()+","+this.getY()+")";
   }
 }
->>>>>>> 9a8606a665c744760f1da3da449935ad9290be85
